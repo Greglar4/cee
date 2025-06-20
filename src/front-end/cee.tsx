d71304cea8c4ff043e84358a9058a1fd76dbb79e
Louis Detweiler <greg.detweiler42@gmail.com>
2025-06-19 19:47:11 -0700

    feat: dummy tauri app
    Creates a dummy app that displays a plotly plot and distributes it as a tauri app.
    
    closes: #1

--- src/front-end/cee.tsx
+++ src/front-end/cee.tsx
@@ -1,5 +1,16 @@
+import { useRef, useEffect } from "react"
+import { createPlot } from "./dummy-line-plot"
+
 export function Cee() {
+    const elementRef = useRef<HTMLDivElement| null>(null)
+    useEffect(() => {
+        if (elementRef.current) {
+            createPlot(elementRef.current)
+        }   
+    }, [])
     return (
-        <div id='myDiv'></div>
+    <>
+        <div ref={elementRef}/>
+    </>  
     )
 }
--- src/front-end/dummy-line-plot.ts
+++ src/front-end/dummy-line-plot.ts
@@ -1,30 +1,34 @@
-import Plotly, { PlotData } from 'plotly.js-dist-min'
+import Plotly from 'plotly.js-dist-min'
+import type { PlotData } from 'plotly.js-dist-min'
 
-const trace1: Partial<PlotData> = {
+export function createPlot(element: Plotly.Root) {
+  const trace1: Partial<PlotData> = {
 
-  x: [1, 2, 3, 4],
+    x: [1, 2, 3, 4],
 
-  y: [10, 15, 13, 17],
+    y: [10, 15, 13, 17],
 
-  type: 'scatter'
+    type: 'scatter'
 
-};
-
-
-const trace2: Partial<PlotData> = {
-
-  x: [1, 2, 3, 4],
-
-  y: [16, 5, 11, 9],
-
-  type: 'scatter'
-
-};
+  };
 
 
-const data: Partial<PlotData>[] = [trace1, trace2];
+  const trace2: Partial<PlotData> = {
+
+    x: [1, 2, 3, 4],
+
+    y: [16, 5, 11, 9],
+
+    type: 'scatter'
+
+  };
 
 
-Plotly.newPlot('myDiv', data).catch(() => {
-  console.log("Plotly plot failed to attach to div")
-});
+  const data: Partial<PlotData>[] = [trace1, trace2];
+
+
+  Plotly.newPlot(element, data).catch(() => {
+    console.log("Plotly plot failed to attach to div")
+  });
+}
+
